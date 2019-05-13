3ef51dff88908336f5d5c25bd1d8d41386f16e5b
NP2 <home@NP2s-iMac-2.local>
2019-05-13 15:27:33 +0430

    Add Indicator Label to parent view

--- HSProgressHUD/ViewController.swift
+++ HSProgressHUD/ViewController.swift
@@ -13,23 +13,33 @@ final class ViewController: UIViewController {
     let shapeLayer = CAShapeLayer()
     let trackShapeLayer = CAShapeLayer()
     
+    let label: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Please Wait..."
+        lbl.textAlignment = .center
+        lbl.textColor = .black
+        lbl.font = UIFont(name: "AvenirNext-Heavy", size: 19)
+        return lbl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        label.frame = CGRect(x: 0, y: 0, width: 140, height: 40)
+        label.center = view.center
+        view.addSubview(label)
         setUI()
         
     }
     
     private func setUI() {
         
-        let center = view.center
-        
-        let circularTrackPath = UIBezierPath(arcCenter: center,
+        let circularTrackPath = UIBezierPath(arcCenter: .zero,
                                              radius: 100,
                                              startAngle: -(.pi) / 2,
                                              endAngle:2 * .pi,
                                              clockwise: true)
-        
+        trackShapeLayer.position = view.center
         trackShapeLayer.path = circularTrackPath.cgPath
         trackShapeLayer.lineCap = CAShapeLayerLineCap.round
         trackShapeLayer.strokeColor = UIColor.purple.withAlphaComponent(0.3).cgColor
@@ -37,12 +47,13 @@ final class ViewController: UIViewController {
         trackShapeLayer.fillColor = UIColor.clear.cgColor
         view.layer.addSublayer(trackShapeLayer)
         
-        let circularPath = UIBezierPath(arcCenter: center,
+        let circularPath = UIBezierPath(arcCenter: .zero,
                                         radius: 100,
                                         startAngle: -(.pi) / 2,
                                         endAngle:2 * .pi,
                                         clockwise: true)
         
+        shapeLayer.position = view.center
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.strokeEnd = 0
         shapeLayer.path = circularPath.cgPath
@@ -54,16 +65,25 @@ final class ViewController: UIViewController {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
-    @objc private func handleTap() {
-        print("SHNDRS")
+    private func beginDownloadingFile() {
+        print("download file")
         
+    }
+    
+    fileprivate func circleAnimation() {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
         basicAnimation.toValue = 1
         basicAnimation.duration = 2
         basicAnimation.fillMode = CAMediaTimingFillMode.forwards
         basicAnimation.isRemovedOnCompletion = false
         shapeLayer.add(basicAnimation, forKey: "shndrs")
+    }
+    
+    @objc private func handleTap() {
+        print("SHNDRS")
+        
+        beginDownloadingFile()
+        circleAnimation()
     }
 }
 
