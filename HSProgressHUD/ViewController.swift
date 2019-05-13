51b33c3dd91ac41d9d0ec0fdd84fb959c03e31ae
NP2 <home@NP2s-iMac-2.local>
2019-05-13 15:08:37 +0430

    create track path and the path itself

--- HSProgressHUD/ViewController.swift
+++ HSProgressHUD/ViewController.swift
@@ -8,27 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    let trackShapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
         
-        let shapeLayer = CAShapeLayer()
+    }
+    
+    private func setUI() {
         
         let center = view.center
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle:2 * .pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
         
+        let circularTrackPath = UIBezierPath(arcCenter: center,
+                                             radius: 100,
+                                             startAngle: -(.pi) / 2,
+                                             endAngle:2 * .pi,
+                                             clockwise: true)
+        
+        trackShapeLayer.path = circularTrackPath.cgPath
+        trackShapeLayer.lineCap = CAShapeLayerLineCap.round
+        trackShapeLayer.strokeColor = UIColor.purple.withAlphaComponent(0.3).cgColor
+        trackShapeLayer.lineWidth = 10
+        trackShapeLayer.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(trackShapeLayer)
+        
+        let circularPath = UIBezierPath(arcCenter: center,
+                                        radius: 100,
+                                        startAngle: -(.pi) / 2,
+                                        endAngle:2 * .pi,
+                                        clockwise: true)
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = 0
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
         shapeLayer.strokeColor = UIColor.purple.cgColor
         shapeLayer.lineWidth = 10
         
         view.layer.addSublayer(shapeLayer)
-        
-        
-        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-
+    
+    @objc private func handleTap() {
+        print("SHNDRS")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "shndrs")
+    }
 }
 
