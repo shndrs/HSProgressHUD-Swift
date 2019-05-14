4ed93588309d1094ecba69992f04afd83b270f2a
NP2 <home@NP2s-iMac-2.local>
2019-05-14 15:56:56 +0430

    HSView Class completed but is not tested yet

--- HSProgressHUD/HSView.swift
+++ HSProgressHUD/HSView.swift
@@ -11,21 +11,18 @@ import UIKit
 fileprivate protocol HSViewHUD {
     func dismiss() -> Void
     func show() -> Void
-    func shapeLayerGenerator() -> Void
+}
+
+fileprivate enum ShapeLayerType {
+    case track, pulsate
 }
 
 public class HSView: UIView {
     
     public static let `default` = HSView()
     
-    private convenience init() {
-        self.init()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+    private var trackLayerColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
+    private var pulsateLayerColor = UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1)
     private lazy var trackShapeLayer = CAShapeLayer()
     private lazy var pulsateShapeLayer = CAShapeLayer()
     private lazy var circularTrackPath = UIBezierPath(arcCenter: .zero,
@@ -52,45 +49,56 @@ public class HSView: UIView {
     }()
     
     
-
+    private convenience init() {
+        self.init()
+    }
     
-    private func layerGenerator(shapeLayer: CAShapeLayer) {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layerGenerator(shapeLayer: CAShapeLayer, type: ShapeLayerType) {
+        
+        switch type {
+        
+        case .track:
+            shapeLayer.strokeColor = trackLayerColor.cgColor
+        case .pulsate:
+            shapeLayer.strokeColor = pulsateLayerColor.withAlphaComponent(0.34).cgColor
+        }
         
         shapeLayer.position = transView.center
         shapeLayer.path = circularTrackPath.cgPath
         shapeLayer.lineCap = CAShapeLayerLineCap.round
         shapeLayer.lineWidth = 20
         shapeLayer.fillColor = UIColor.clear.cgColor
-        transView.layer.addSublayer(trackShapeLayer)
+        transView.layer.addSublayer(shapeLayer)
     }
 
 }
 
-// MARK: Life Cycle
+// MARK: - Life Cycle
 
 extension HSView {
     
     public override func awakeFromNib() {
-        
         titleLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 70)
         titleLabel.center = transView.center
         transView.addSubview(titleLabel)
         setUI()
     }
-    
-    
 }
 
-// MARK: Show and Dismiss HSProgressHUD
+// MARK: - Show and Dismiss HSProgressHUD
 
-extension HSView {
+extension HSView: HSViewHUD {
     
     /// Show HSProgressHUD with fadeIn animation
     
     public func show() {
         
         transView.alpha = 0
-        transView.bringSubviewToFront(<#T##view: UIView##UIView#>)
+//        transView.bringSubviewToFront(T##view: UIView##UIView)
         UIApplication.shared.keyWindow?.addSubview(transView)
         
         UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseInOut, animations: {
@@ -108,24 +116,24 @@ extension HSView {
         UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseInOut, animations: {
             self.alpha = 0
             self.transView.layoutIfNeeded()
-        })
-        
-        transView.alpha = 0
+        }) { (success) in
+            self.transView.removeFromSuperview()
+        }
     }
 }
 
-// MARK: Set UI
+// MARK: - Set UI
 
 extension HSView {
     
     private func setUI() {
-        
+        layerGenerator(shapeLayer: pulsateShapeLayer, type: .pulsate)
+        setAnimations()
+        layerGenerator(shapeLayer: trackShapeLayer, type: .track)
     }
-    
-    
 }
 
-// MARK: Customization
+// MARK: - Customization
 
 public extension HSView {
     
@@ -160,4 +168,41 @@ public extension HSView {
                                          endAngle:2 * .pi,
                                          clockwise: true)
     }
+}
+
+// MARK: - Animations
+
+extension HSView {
+    
+    fileprivate func setAnimations() {
+        
+        let transformAnimation = CABasicAnimation(keyPath: "transform.scale")
+        let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+        let tarnsformXScaleAnimation = CABasicAnimation(keyPath: "transform.scale.x")
+        
+        tarnsformXScaleAnimation.fromValue = 1
+        tarnsformXScaleAnimation.toValue = 1.02
+        tarnsformXScaleAnimation.duration = 1.0
+        tarnsformXScaleAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        tarnsformXScaleAnimation.autoreverses = true
+        tarnsformXScaleAnimation.repeatCount = .infinity
+        
+        opacityAnimation.fromValue = 1
+        opacityAnimation.toValue = 0.5
+        opacityAnimation.duration = 0.4
+        opacityAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeIn)
+        opacityAnimation.autoreverses = true
+        opacityAnimation.repeatCount = .infinity
+        
+        transformAnimation.toValue = 1.13
+        transformAnimation.duration = 0.8
+        transformAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut )
+        transformAnimation.autoreverses = true
+        transformAnimation.repeatCount = .infinity
+        
+        pulsateShapeLayer.add(transformAnimation, forKey: "shndrsTransformKey")
+        titleLabel.layer.add(opacityAnimation, forKey: "shndrsOpacityKey")
+        titleLabel.layer.add(tarnsformXScaleAnimation, forKey: "shndrsXTransKey")
+    }
+  
 }
