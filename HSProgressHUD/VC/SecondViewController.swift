3b42f96064932b6c29212de83775402b0a95a9de
NP2 <sahandraeisi1994@gmail.com>
2019-06-11 16:03:35 +0430

    Added LordOfTheRing example to the project

--- HSProgressHUD/VC/SecondViewController.swift
+++ HSProgressHUD/VC/SecondViewController.swift
@@ -50,6 +50,25 @@ final class SecondViewController: UIViewController {
         }
     }
     
+    @IBAction private func lordOfTheRingsButtonPressed(_ sender: UIButton) {
+        
+        let progress = HSProgressOptions { (options) in
+            options.radius = 100
+            options.secondLayerStrokeColor = .brown
+            options.strokeWidth = 17
+            options.thirdLayerStrokeColor = .gray
+            options.firstLayerStrokeColor = .darkGray
+            options.title = "shndrs presents"
+            options.animationOption = HSAnimationOptions.lineLayer
+        }
+        
+        let hud = HSProgressHUD.set(progress: progress)
+        hud.show()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            hud.dismiss(completion: nil)
+        }
+    }
     
     
 }
